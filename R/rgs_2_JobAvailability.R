54f75fc7c4e1655bdaf4c25d2958aa8599e053b9
Kaiqing Wang <harveywang1992@gmail.com>
2017-08-18 13:17:34 +0800

    Modify for 2

--- R/rgs_2_JobAvailability.R
+++ R/rgs_2_JobAvailability.R
@@ -22,7 +22,7 @@ execIndicatorJobAvailability <- function(){
   
   # the follow two lines are for testing
   boundary_url = "http://115.146.93.46:8080/geoserver/Geographic_Boundaries/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Geographic_Boundaries:qld_sa2_2011_aust&outputFormat=JSON&cql_filter=gcc_code11=%273GBRI%27"
-  employmentSector_url = "http://115.146.92.236:8080/geoserver/group2/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=group2:qld_employment_by_sector_aus_sa2_2011_ste_gccsa_info&outputFormat=json&cql_filter=gcc_code11=%273GBRI%27"
+  #employmentSector_url = "http://115.146.92.236:8080/geoserver/group2/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=group2:qld_employment_by_sector_aus_sa2_2011_ste_gccsa_info&outputFormat=json&cql_filter=gcc_code11=%273GBRI%27"
   employmentCategory_url = "http://115.146.92.236:8080/geoserver/group2/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=group2:qld_employment_categories_aus_sa2_2011_ste_gccsa_info&outputFormat=json&cql_filter=gcc_code11=%273GBRI%27"
   
   # load spatial object direct from geojson
@@ -33,12 +33,12 @@ execIndicatorJobAvailability <- function(){
     return(FALSE)
   }
   
-  employmentSector = utils.loadGeoJSON2DF(employmentSector_url)
+  #employmentSector = utils.loadGeoJSON2DF(employmentSector_url)
   # check if data layer can be successfully loaded
-  if(is.null(employmentSector)){
-    utils.debugprint("fail to load data layer for employment(sector)")
-    return(FALSE)
-  }
+ # if(is.null(employmentSector)){
+  #  utils.debugprint("fail to load data layer for employment(sector)")
+  #  return(FALSE)
+  #}
   
   employmentCategory = utils.loadGeoJSON2DF(employmentCategory_url)
   # check if data layer can be successfully loaded
@@ -48,14 +48,14 @@ execIndicatorJobAvailability <- function(){
   }
   
   # calculation
-  mergedData = merge.data.frame(x=boundary@data, y=employmentSector, by.x = "sa2_main11", by.y = "sa2_main11", sort = FALSE)
-  mergedDataAll = merge.data.frame(x=mergedData, y=employmentCategory, by.x = "sa2_main11", by.y = "sa2_main11", sort = FALSE)
-  boundary@data = mergedDataAll
+  mergedData = merge.data.frame(x=boundary@data, y=employmentCategory, by.x = "sa2_main11", by.y = "sa2_main11", sort = FALSE)
+  #mergedDataAll = merge.data.frame(x=mergedData, y=employmentCategory, by.x = "sa2_main11", by.y = "sa2_main11", sort = FALSE)
+  boundary@data = mergedData
   
   boundary@data$albers_sqm <- NULL
   
   boundary@data$job_availability = 0.0
-  boundary@data$job_availability <- with(boundary@data, total.x/total.y)
+  boundary@data$job_availability <- with(boundary@data, (employed_full_time+employed_part_time+employed_away_from_work+employed_hours_of_work_not_stated)/total)
   
   # this example shows how to publish a geolayer by creating multiple wms styles on various attributes of the same data layer. 
   # the data layer will be only published one time, with various wms styles generated for selected attributes 
